a48f14e063c0cfc949cb248c422cb2616d6771e2
anthonyApptist <anthony@theapptist.com>
2017-02-21 16:51:42 -0500

    Firebase

---
+++ ScrimIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrimIT
+//
+//  Created by Anthony Ma on 21/2/2017.
+//  Copyright © 2017 Anthony Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
